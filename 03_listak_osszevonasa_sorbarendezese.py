0881c210017d11db02e7a8285e160dc358b38ad6
molnarmaxim <molnar.maxim@diak.szbi-pg.hu>
2024-11-20 13:28:34 +0100

    Update 03_listak_osszevonasa_sorbarendezese.py

--- 03_listak_osszevonasa_sorbarendezese.py
+++ 03_listak_osszevonasa_sorbarendezese.py
@@ -5,3 +5,10 @@ Egyesítsd a két listát, és rendezd a lista elemeit növekvő sorrendbe!
 
 
 # írasd ki az első és az utolsó elemet!
+lista_egy = [3, 1, 4]
+lista_ketto = [9, 7, 2]
+
+print(lista_egy)
+lista_egy.extend(lista_ketto)
+print(lista_egy)
+print(sorted(lista_egy))
