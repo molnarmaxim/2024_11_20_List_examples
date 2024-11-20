ecf0ec43d42e8fde989c7cfc828ee4fe1ea65ed6
molnarmaxim <molnar.maxim@diak.szbi-pg.hu>
2024-11-20 13:38:50 +0100

    Update 01_lista_elemeinek_megszamolasa.py

--- 01_lista_elemeinek_megszamolasa.py
+++ 01_lista_elemeinek_megszamolasa.py
@@ -8,10 +8,8 @@ Határozd meg a lista hosszát egy ciklus segítségével
 lista = [5, 8, 12, 15, 22]
 
 hossz = 0
-for i in range(5):
-    print(lista[i])
-    i = i + 1
+for elem in lista:
     hossz = hossz + 1
 
-print(f"Lista elemei len ciklussal: {len(lista)}")
-print(f"A lista hossza: {hossz}")
+print(f"Lista elemei len methódussal: {len(lista)}")
+print(f"A lista hossza ciklussal: {hossz}")
