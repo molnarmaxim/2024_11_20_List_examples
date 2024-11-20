73e0e14dfc2bda0d34cd03703e5b8c1fd4a7c65d
molnarmaxim <molnar.maxim@diak.szbi-pg.hu>
2024-11-20 13:28:14 +0100

    Update 02_lista_elemeinek_szorzata.py

--- 02_lista_elemeinek_szorzata.py
+++ 02_lista_elemeinek_szorzata.py
@@ -2,3 +2,7 @@
 Hozz létre egy listát számokkal: [2, 4, 6, 8]. 
 Írd ki a lista elemeinek szorzatát (azaz a számok szorzatát)!
 """
+
+lista = [2, 4, 6, 8]
+print(lista[0] * lista[1] * lista[2] * lista[3])
+
